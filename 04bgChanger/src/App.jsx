bf82e38781ad0876ef22defa41f0e8309b33bfc5
Vivek-aDev <vivekananda.work2022@gmail.com>
2024-01-17 12:04:25 +0530

    background color changer

---
+++ 04bgChanger/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [color, setColor] = useState("gray");
+
+  // function changeColor(color) {
+  //   setColor(color);
+  // }
+
+  return (
+    <>
+      <div
+        className="w-full h-screen duration-200"
+        style={{ backgroundColor: color }}
+      >
+        <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+          <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+            <button
+              onClick={() => setColor("orangered")}
+              className="outline-none px-4 py-1 rounded-full shadow-lg text-black"
+              style={{ backgroundColor: "orangered" }}
+            >
+              Orange
+            </button>
+            <button
+              onClick={() => setColor("green")}
+              className="outline-none px-4 py-1 rounded-full shadow-lg text-black"
+              style={{ backgroundColor: "green" }}
+            >
+              Green
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
